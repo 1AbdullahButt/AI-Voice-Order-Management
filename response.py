d422a0e76ff4144eeccd04fb2476edd3c0950158
Muhammad Abdullah Butt <168034895+1AbdullahButt@users.noreply.github.com>
2025-10-19 18:04:27 +0500

    Delete response.py

--- response.py
+++
@@ -1,5 +0,0 @@
-def generate_response(intent):
-    if isinstance(intent, dict):
-        return intent.get("response", {}).get("message", "No response message provided.")
-    else:
-        return "Invalid intent format."
